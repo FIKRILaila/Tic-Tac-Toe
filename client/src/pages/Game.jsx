c844c72d0daf2d57184134eb465a9911b315e653
FIKRILaila <77831678+FIKRILaila@users.noreply.github.com>
2022-03-18 14:50:18 +0100

    update

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import React,{useEffect} from 'react'
+import io from 'socket.io-client';
+let socket;
 const Game = ({name, gameId}) => {
-    const event = gameId ? 'Join Game' : 'Create Game';
-    console.log(`${name} want to ${event}`);
+  const SERVER_ENDPOINT = 'http://localhost:5001';
+  const event = gameId ? 'Join Game' : 'Create Game';
+  console.log(`${name} want to ${event}`);
+
+  useEffect(() =>{
+    socket = new io(SERVER_ENDPOINT);
+  });
   return (
     <div>Game</div>
   );
--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,12 @@
 const http = require('http');
+const socketIO = require('socket.io');
 const server = http.createServer();
+const io = socketIO(server);
 const PORT = process.env.PORT || 5000;
+io.on('connection',socket => {
+    console.log('New connection')
+});
+
 server.listen(PORT, () => {
     console.log(`listen to ${PORT}`);
 })
